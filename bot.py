e6530a40f2825a6013d2ca6067a0eaed93c91fc2
Arushi Mittal <iarushimittal@gmail.com>
2021-10-12 21:41:09 +0530

    Added bot counter function

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 import random
+import json
+import requests
 
 TOKEN = "#"
 
@@ -8,6 +10,19 @@ bot = commands.Bot(command_prefix='!')
 @bot.command(name='faq', help='Searches the FAQ for the most relevant section corresponding to the provided keyword.')
 async def faq(ctx, keyword):
     response = keyword
+    await ctx.send(response)
+
+def count (author) :
+    '''
+    A black box function that will depend on the implementation of the 
+    post question feature to count number of problems solved by a user.
+    '''
+    solvedCount = 0
+    return solvedCount
+
+@bot.command (name = 'counter', help = 'Counts the number of questions solved by the user')
+async def faq(ctx):
+    response = f"{ctx.message.author.mention} you have solved {count(ctx.message.author)} problems"
     await ctx.send(response)
 
 
@@ -22,7 +37,6 @@ async def on_message(message):
     greetings = ['Hey! How are you?', 'Hello! Glad to see you.']
     await message.channel.send(random.choice(greetings))
 
-bot.run(TOKEN)
 
 def get_qoute():
     response = requests.get("https://zenquotes.io/api/random")
@@ -41,4 +55,3 @@ async def on_message(message):
         await message.channel.send(response)
 
 bot.run(TOKEN)
-
